449d058f620a587ffab3db98b81646e7db920e75
Carlos Quispe <carlosqe2000@gmail.com>
2022-08-31 17:25:32 -0300

    arreglos despues del after

---
+++ funciones.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+
+const data = fs.readFileSync("./Data/productos.json", "utf-8");
+
+class Datos {
+
+    constructor(archivo) {
+        this.archivo = archivo
+    }
+
+    async save(producto) {
+        try {
+            const data = await fs.promises.readFile(`${this.archivo}/productos.json`, `utf-8`)
+            const productos = JSON.parse(data);
+            const id = productos.length + 1;
+            producto.id = id;
+            productos.push(producto);
+            const productosString = JSON.stringify(productos);
+            await fs.promises.writeFile(`${this.archivo}/productos.json`, productosString)
+            return "Se agrego comida su id es "+producto.id
+
+        } catch (error) {
+            return "no se pudo leer el archvo"
+
+        }
+    }
+
+    async getById(id) {
+        
+
+        const data = await fs.promises.readFile(`${this.archivo}/productos.json`,"utf-8");
+        const productos = JSON.parse(data);
+        const producto = productos.find((producto) => producto.id == id);
+        if (producto) {
+            return producto;
+        } else {
+            throw new Error("Producto no encontrado");
+        }
+
+
+    }
+
+    async random(){
+        try{
+            const data = await fs.promises.readFile(`${this.archivo}/productos.json`, "utf-8")
+            const dataArray = JSON.parse(data);
+            const aleatorio = Math.floor(Math.random()*dataArray.length);
+            const valor = dataArray[aleatorio]
+
+            return valor
+
+        }catch{
+            return "ocurrio un error inesperado"
+
+        }
+
+    }
+    async deleteById(id) {
+            
+
+        try {
+            const data = await fs.promises.readFile(`${this.archivo}/productos.json`, "utf-8")
+            const productos = JSON.parse(data);
+            const arrayBorrado = productos.filter((item) => item.id !== id)
+            const verricar = productos.find((item) => item.id===id);
+            if(verricar) {
+                await fs.promises.writeFile(`${this.archivo}/productos.json`, JSON.stringify(arrayBorrado))
+                return "se borro el archivo correctamente"
+            }else{
+                return "no se encontro el archivo"
+            }
+            // await fs.promises.writeFile(`${this.archivo}/productos.json`, JSON.stringify(arrayBorrado))
+            // return "se borro el archivo"
+
+        } catch (error){
+            return "error el leer el archivo borrado por id"
+        }
+
+    }
+    async getAll() {
+        try {
+            const data = await fs.promises.readFile(`${this.archivo}/productos.json`, "utf-8")
+            const datos = JSON.parse(data)
+            return datos
+
+        } catch (error){
+            return "error al leer el archivo"
+        }
+
+
+    }
+    async deleteAll() {
+        try {
+            //creo q se refiere a esto no?
+            // fs.unlinkSync(`${this.archivo}/productos.json`, "utf-8")
+            fs.promises.writeFile(`${this.archivo}/productos.json`,JSON.stringify([]))
+
+            console.log('se borro la lista')
+        } catch (err) {
+            console.error("ocurrio un error", err)
+        }
+
+    }
+
+
+}
+async function start() {
+    const db = new Datos("Data");
+
+    console.log(await db.save({
+        "title": "goma",
+        "precio": 5
+       }))
+    console.log(await db.getAll())
+    //console.log(await db. para ver probar las diferentes funciones)
+
+}
+
+
+module.exports = Datos;
--- index.js
+++ index.js
@@ -1,49 +1,74 @@
-const fs = require('fs');
+// const fs = require('fs');
 const express = require('express');
-const { response, request } = require('express');
-
+const db = require("./funciones.js")
+const DB = new db("Data")
 const app = express();
-app.get("/", async (req,res) => {
 
-    try{
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+
+    try {
         res.send(`<h1>HOLA TAREA 3</h1>`)
 
-    }catch(e){
+    } catch (e) {
         res.end(e)
+    }
+})
+
+app.get("/productos", async (req, res) => {
+    try {
+        const data = await DB.getAll()
+        res.send(data)
+        
+
+    } catch (e) {
+        return res.send("ERROR")
     }
 
 })
-app.get("/productos", async (req,res) => {
 
-    try{
-        const data = await fs.promises.readFile("./productos.json", "utf-8")
-        const productos = JSON.parse(data)
-        res.send(productos)
+app.get("/producto", async (req, res) => {
+    const {id} = req.query
+    try {
+        const data = await DB.getById(id)
+        return res.send(data)
 
-    }catch(e){
-        console.log(e);
-        res.end(e)
+    } catch (e) {
+        return res.status(404).send({
+            error: true,
+            msg: e.message
+        })
+
     }
-
 })
-app.get("/productoRamdom", async(req, res) => {
-    try{
-        const data = await fs.promises.readFile("./productos.json", "utf-8")
-        const dataArray = JSON.parse(data);
-        const aleatorio = Math.floor(Math.random()*dataArray.length);
-        const valor = dataArray[aleatorio]
-        res.send(valor)
-    }catch(e){
+
+app.post("/producto", async (req, res) => {
+    
+        const {nombre,correo} = req.body;
+
+        const data = await DB.createUser({nombre,correo});
+        return res.send({
+            error: false,
+            msg: "Usuario creado"
+        });
+    
+})
+
+
+app.get("/productoRamdom", async (req, res) => {
+    try {
+        
+        const data = await DB.random()
+        res.send(data)
+
+
+    } catch (e) {
         console.log("funciona mal")
 
-    }    
+    }
 })
 
-const server = app.listen(8080,()=>{
+const server = app.listen(8080, () => {
     console.log(`servidor express iniciado`)
-})
-
-server.on("error",()=>{
-    //on escuchar masomenos 
-    console.log(`error ${error}`)
 })
